ff6e0f2b465b4a2f1c017af1eb232e8a48afcbe9
MatDls <mdzielinski@gmail.com>
2017-12-09 00:46:57 +0100

    Code should not be written like this It's too complicated and upreadeable. It should be simple and clear. I push this but I don't understand it myself, and it still doesn't work.

--- src/main/java/PPJ/Task5/Task.java
+++ src/main/java/PPJ/Task5/Task.java
@@ -4,19 +4,17 @@ public class Task {
 	public static void main(String[] args) {
 		int[] tab = {1, 0, 0, 0, 2, 0, 0, 0, 3};
 
-		for (int i = 0; i < 3; i++) {
-			System.out.println((i + 1) + "\t" + loop(tab, i * 3, 3, 1));
+		for (int i = 0; i < 9; i++) {
+			System.out.println(loop(tab, ((i % 3) * 3), tab.length/3, 1, (i % 3 + 1) + "\t"));
 		}
 	}
 
-	private static String loop(int[] tab, int start, int stop, int dir) {
-		int i = start;
-		int j = stop;
-		String line = "";
-		while (1 <= j) {
-			line += "\t" + tab[i];
-			i += dir;
-			j--;
+	private static String loop(int[] tab, int start, int length, int dir, String line) {
+
+		while (1 <= length) {
+			line += " " + tab[start];
+			start += dir;
+			length--;
 		}
 		return line;
 	}
